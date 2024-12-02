a1321d5e927e3d63c2ef98ee411d7cfdebac2a6a
thinh.le <thinh.le@hellohealthgroup.com>
2024-12-02 10:45:53 +0700

    update

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -21,20 +21,24 @@ func main() {
 	out := ""
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-		inp, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		tokens := strings.Split(inp, " ")
-		switch tokens[0] {
-		case constants.EXIT:
-			code := 0
-			if len(tokens) > 1 {
-				code, _ = strconv.Atoi(tokens[1])
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			inp := scanner.Text()
+			tokens := strings.Split(inp, " ")
+			fmt.Println(strconv.Quote(inp))
+			switch tokens[0] {
+			case constants.EXIT:
+				code := 0
+				if len(tokens) > 1 {
+					code, _ = strconv.Atoi(tokens[1])
+				}
+				os.Exit(code)
+			case constants.ECHO:
+				out = strings.Join(tokens[1:], " ")
+			default:
+				out = fmt.Sprintf("%v: command not found", tokens[0])
 			}
-			os.Exit(code)
-		case constants.ECHO:
-			out = strings.Join(tokens[1:], " ")
-		default:
-			out = fmt.Sprintf("%v: command not found", tokens[0])
+			fmt.Fprint(os.Stdout, out, "\n")
 		}
-		fmt.Fprint(os.Stdout, strings.TrimSuffix(out, "\n"), "\n")
 	}
 }
