28649d314e05f3445c52452ce4fc6277104aea99
thinh.le <thinh.le@hellohealthgroup.com>
2024-12-02 09:49:19 +0700

    update

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,7 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
+
+	"github.com/codecrafters-io/shell-starter-go/constants"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
@@ -12,12 +15,22 @@ var _ = fmt.Fprint
 
 func main() {
 	// Uncomment this block to pass the first stage
-
+	// inpChan := make(chan string)
+	// outputChan := shell.NewTokenizer(inpChan)
 	// Wait for user input
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		inp, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		str := fmt.Sprintf("%v: command not found\n", strings.TrimRight(inp, "\n"))
-		fmt.Fprint(os.Stdout, str)
+		tokens := strings.Split(inp, " ")
+		if tokens[0] == constants.EXIT {
+			code := 0
+			if len(tokens) > 1 {
+				code, _ = strconv.Atoi(tokens[1])
+			}
+			os.Exit(code)
+		} else {
+			str := fmt.Sprintf("%v: command not found\n", strings.TrimRight(inp, "\n"))
+			fmt.Fprint(os.Stdout, str)
+		}
 	}
 }
