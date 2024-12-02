aa1b0210d24f4843c76996907d696a582168fc41
thinh.le <thinh.le@hellohealthgroup.com>
2024-12-02 09:34:46 +0700

    update

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -12,10 +12,10 @@ var _ = fmt.Fprint
 
 func main() {
 	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
 	for {
+		fmt.Fprint(os.Stdout, "$ ")
 		inp, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 		str := fmt.Sprintf("%v: command not found\n", strings.TrimRight(inp, "\n"))
 		fmt.Fprint(os.Stdout, str)
