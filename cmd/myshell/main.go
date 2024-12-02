d4422f1dd8b662e5aa94e69a042d2cbf64f8e27b
thinh.le <thinh.le@hellohealthgroup.com>
2024-12-02 10:35:02 +0700

    update

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -30,7 +30,7 @@ func main() {
 			}
 			os.Exit(code)
 		case constants.ECHO:
-			fmt.Fprint(os.Stdout, strings.TrimSpace(inp[len(tokens):]))
+			fmt.Fprint(os.Stdout, strings.TrimRight(strings.TrimSpace(inp[len(tokens):]), "\n"))
 		default:
 			str := fmt.Sprintf("%v: command not found\n", strings.TrimRight(inp, "\n"))
 			fmt.Fprint(os.Stdout, str)
