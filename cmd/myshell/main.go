621553b12c91fdedfa8fb63343404f3223ad0e98
thinh.le <thinh.le@hellohealthgroup.com>
2024-12-02 09:25:22 +0700

    update

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -14,5 +14,6 @@ func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	inp, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	fmt.Printf("%v: command not found", inp)
 }
