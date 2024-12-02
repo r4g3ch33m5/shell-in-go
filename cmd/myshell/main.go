f0519e420fb81b833dc4a64d241e501747d47e16
thinh.le <thinh.le@hellohealthgroup.com>
2024-12-02 13:39:58 +0700

    update

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -18,6 +18,7 @@ var _ = fmt.Fprint
 
 func readEntry(entry fs.DirEntry, prefix string, level int) {
 	curEntry := filepath.Join(prefix, entry.Name())
+	fmt.Println(strings.Repeat("\t", level), entry.Name(), entry.IsDir(), constants.MapCommand2Path[entry.Name()] != constants.BUILTIN)
 	if entry.IsDir() {
 		childEntries, _ := os.ReadDir(entry.Name())
 		for _, entry := range childEntries {
